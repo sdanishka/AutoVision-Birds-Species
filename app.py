f87a6bd4842f63caa6b4e3916e4c4cc995ab1876
Danish Afridi <76814231+sdanishka@users.noreply.github.com>
2024-05-23 15:46:09 +0500

    Files Updated

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from io import BytesIO
 from PIL import Image
 import os
 
-import boto3
-from botocore import UNSIGNED  # contact public s3 buckets anonymously
-from botocore.client import Config  # contact public s3 buckets anonymously
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -25,7 +21,7 @@ def load_model(path: str = 'models/trained_model_resnet50.pt') -> ResnetModel:
 @st.cache_data()
 def load_index_to_label_dict(
         path: str = 'src/index_to_class_label.json'
-        ) -> dict:
+) -> dict:
     """Retrieves and formats the
     index to class label
     lookup dictionary needed to
@@ -39,24 +35,26 @@ def load_index_to_label_dict(
     return index_to_class_label_dict
 
 
-def load_files_from_s3(
+def load_files_from_local(
         keys: list,
-        bucket_name: str = 'bird-classification-bucket'
-        ) -> list:
-    """Retrieves files from S3 bucket"""
-    s3 = boto3.client('s3')
-    s3_files = []
+        base_path: str = 'local_images_folder'
+) -> list:
+    """Retrieves files from local directories"""
+    local_files = []
     for key in keys:
-        s3_file_raw = s3.get_object(Bucket=bucket_name, Key=key)
-        s3_file_cleaned = s3_file_raw['Body'].read()
-        s3_file_image = Image.open(BytesIO(s3_file_cleaned))
-        s3_files.append(s3_file_image)
-    return s3_files
+        try:
+            local_file_path = os.path.join(base_path, key)
+            local_file_image = Image.open(local_file_path)
+            local_files.append(local_file_image)
+            # st.write(f"Successfully loaded {key}")
+        except Exception as e:
+            st.error(f"Error loading {key} from local path: {e}")
+    return local_files
 
 
 @st.cache_data()
-def load_s3_file_structure(path: str = 'src/all_image_files.json') -> dict:
-    """Retrieves JSON document outining the S3 file structure"""
+def load_local_file_structure(path: str = 'src/all_image_files.json') -> dict:
+    """Retrieves JSON document outlining the local file structure"""
     with open(path, 'r') as f:
         return json.load(f)
 
@@ -66,20 +64,20 @@ def load_list_of_images_available(
         all_image_files: dict,
         image_files_dtype: str,
         bird_species: str
-        ) -> list:
+) -> list:
     """Retrieves list of available images given the current selections"""
     species_dict = all_image_files.get(image_files_dtype)
     list_of_files = species_dict.get(bird_species)
     return list_of_files
 
 
-@st.cache_data()
+# @st.cache_data()
 def predict(
         img: Image.Image,
         index_to_label_dict: dict,
         model,
         k: int
-        ) -> list:
+) -> list:
     """Transforming input image according to ImageNet paper
     The Resnet was initially trained on ImageNet dataset
     and because of the use of transfer learning, I froze all
@@ -100,7 +98,7 @@ def predict(
 if __name__ == '__main__':
     model = load_model()
     index_to_class_label_dict = load_index_to_label_dict()
-    all_image_files = load_s3_file_structure()
+    all_image_files = load_local_file_structure()
     types_of_birds = sorted(list(all_image_files['test'].keys()))
     types_of_birds = [bird.title() for bird in types_of_birds]
 
@@ -123,19 +121,17 @@ if __name__ == '__main__':
         img = Image.open(file)
         prediction = predict(img, index_to_class_label_dict, model, k=5)
         top_prediction = prediction[0][0]
-        available_images = all_image_files.get(
-            'train').get(top_prediction.upper())
+        available_images = all_image_files.get('train').get(top_prediction.upper())
         examples_of_species = np.random.choice(available_images, size=3)
-        files_to_get_from_s3 = []
+        files_to_get_from_local = []
 
         for im_name in examples_of_species:
             path = os.path.join('train', top_prediction.upper(), im_name)
-            files_to_get_from_s3.append(path)
-        images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
+            files_to_get_from_local.append(path)
+        images_from_local = load_files_from_local(keys=files_to_get_from_local)
 
     else:
-        dataset_type = st.sidebar.selectbox(
-            "Data Portion Type", data_split_names)
+        dataset_type = st.sidebar.selectbox("Data Portion Type", data_split_names)
         image_files_subset = dtype_file_structure_mapping[dataset_type]
 
         selected_species = st.sidebar.selectbox("Bird Type", types_of_birds)
@@ -143,37 +139,40 @@ if __name__ == '__main__':
             all_image_files, image_files_subset, selected_species.upper())
         image_name = st.sidebar.selectbox("Image Name", available_images)
         if image_files_subset == 'consolidated':
-            s3_key_prefix = 'consolidated/consolidated'
+            local_key_prefix = 'consolidated/consolidated'
         else:
-            s3_key_prefix = image_files_subset
-        key_path = os.path.join(
-            s3_key_prefix, selected_species.upper(), image_name)
-        files_to_get_from_s3 = [key_path]
+            local_key_prefix = image_files_subset
+        key_path = os.path.join(local_key_prefix, selected_species.upper(), image_name)
+        files_to_get_from_local = [key_path]
         examples_of_species = np.random.choice(available_images, size=3)
 
         for im in examples_of_species:
-            path = os.path.join(s3_key_prefix, selected_species.upper(), im)
-            files_to_get_from_s3.append(path)
-        images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
-        img = images_from_s3.pop(0)
-        prediction = predict(img, index_to_class_label_dict, model, 5)
+            path = os.path.join(local_key_prefix, selected_species.upper(), im)
+            files_to_get_from_local.append(path)
+        images_from_local = load_files_from_local(keys=files_to_get_from_local)
+        if images_from_local:  # Check if the list is not empty
+            img = images_from_local.pop(0)
+            prediction = predict(img, index_to_class_label_dict, model, 5)
+        else:
+            st.error("No images retrieved from local path. Please check your local configuration and keys.")
+            img = None
 
-    st.title("Here is the image you've selected")
-    resized_image = img.resize((336, 336))
-    st.image(resized_image)
-    st.title("Here are the five most likely bird species")
-    df = pd.DataFrame(data=np.zeros((5, 2)),
-                      columns=['Species', 'Confidence Level'],
-                      index=np.linspace(1, 5, 5, dtype=int))
+    if img:
+        st.title("Here is the image you've selected")
+        resized_image = img.resize((336, 336))
+        st.image(resized_image)
+        st.title("Here are the five most likely bird species")
+        df = pd.DataFrame(data=np.zeros((5, 2)),
+                          columns=['Species', 'Confidence Level'],
+                          index=np.linspace(1, 5, 5, dtype=int))
 
-    for idx, p in enumerate(prediction):
-        link = 'https://en.wikipedia.org/wiki/' + \
-            p[0].lower().replace(' ', '_')
-        df.iloc[idx,
-                0] = f'<a href="{link}" target="_blank">{p[0].title()}</a>'
-        df.iloc[idx, 1] = p[1]
-    st.write(df.to_html(escape=False), unsafe_allow_html=True)
-    st.title(f"Here are three other images of the {prediction[0][0]}")
+        for idx, p in enumerate(prediction):
+            link = 'https://en.wikipedia.org/wiki/' + p[0].lower().replace(' ', '_')
+            df.iloc[idx, 0] = f'<a href="{link}" target="_blank">{p[0].title()}</a>'
+            df.iloc[idx, 1] = p[1]
+        st.write(df.to_html(escape=False), unsafe_allow_html=True)
+        st.title(f"Here are three other images of the {prediction[0][0]}")
 
-    st.image(images_from_s3)
-    # st.title('How it works:')
+        st.image(images_from_local)
+    else:
+        st.write("No image to display.")
